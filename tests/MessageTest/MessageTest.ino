551e0c80801a96666fd7cfff6c30432bbe4e2fc5
Philippe Muller <philippe.muller@gmail.com>
2024-01-25 18:17:08 +0800

    test(message): encode a message with a single record

--- tests/MessageTest/MessageTest.ino
+++ tests/MessageTest/MessageTest.ino
@@ -10,6 +10,21 @@ test(message_constructor)
   assertEqual(message.get_encoded_size(), (uint32_t)0);
 }
 
+test(message_add_one_record)
+{
+  NdefMessage message;
+  NdefRecord *record = new NdefRecord();
+  message.add_record(record);
+  assertEqual(message.get_record_count(), (uint8_t)1);
+  assertEqual(message.get_encoded_size(), (uint32_t)3);
+  uint8_t *encoded = message.encode();
+  // MB=1, ME=1, CF=0, SR=1, IL=0, TNF=0x00
+  assertEqual(encoded[0], (uint8_t)0b11010000);
+  assertEqual(encoded[1], (uint8_t)0);
+  assertEqual(encoded[2], (uint8_t)0);
+  free(encoded);
+}
+
 void setup()
 {
 #if !defined(EPOXY_DUINO)
