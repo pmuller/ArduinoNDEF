5a54929bd7b7b9724ed9df456edc3e2ece6a80f9
Philippe Muller <philippe.muller@gmail.com>
2024-01-25 18:20:44 +0800

    test(message): encode a message with 3 records

--- tests/MessageTest/MessageTest.ino
+++ tests/MessageTest/MessageTest.ino
@@ -25,6 +25,33 @@ test(message_add_one_record)
   free(encoded);
 }
 
+test(message_add_three_records)
+{
+  NdefMessage message;
+  NdefRecord *record1 = new NdefRecord();
+  NdefRecord *record2 = new NdefRecord();
+  NdefRecord *record3 = new NdefRecord();
+  message.add_record(record1);
+  message.add_record(record2);
+  message.add_record(record3);
+  assertEqual(message.get_record_count(), (uint8_t)3);
+  assertEqual(message.get_encoded_size(), (uint32_t)9);
+  uint8_t *encoded = message.encode();
+  // MB=1, ME=0, CF=0, SR=1, IL=0, TNF=0x00
+  assertEqual(encoded[0], (uint8_t)0b10010000);
+  assertEqual(encoded[1], (uint8_t)0);
+  assertEqual(encoded[2], (uint8_t)0);
+  // MB=0, ME=0, CF=0, SR=1, IL=0, TNF=0x00
+  assertEqual(encoded[3], (uint8_t)0b00010000);
+  assertEqual(encoded[4], (uint8_t)0);
+  assertEqual(encoded[5], (uint8_t)0);
+  // MB=0, ME=1, CF=0, SR=1, IL=0, TNF=0x00
+  assertEqual(encoded[6], (uint8_t)0b01010000);
+  assertEqual(encoded[7], (uint8_t)0);
+  assertEqual(encoded[8], (uint8_t)0);
+  free(encoded);
+}
+
 void setup()
 {
 #if !defined(EPOXY_DUINO)
