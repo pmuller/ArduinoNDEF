fb94cbc71aa2e9ecd8792b8fce249e0953693edd
Philippe Muller <philippe.muller@gmail.com>
2024-01-27 18:57:43 +0800

    feat: run valgrind with --track-origins=yes

--- tests/test.mk
+++ tests/test.mk
@@ -3,6 +3,6 @@ ARDUINO_LIBS := AUnit MFRC522v2-NDEF
 include ../../../EpoxyDuino/EpoxyDuino.mk
 
 valgrind_run:
-	valgrind --error-exitcode=1 --leak-check=full ./$(APP_NAME).out
+	valgrind --error-exitcode=1 --leak-check=full --track-origins=yes ./$(APP_NAME).out
 
 test: valgrind_run
