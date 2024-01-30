96510dabf14749a2e60b5fbf5d38262826af6d9f
Philippe Muller <philippe.muller@gmail.com>
2024-01-30 10:34:43 +0800

    feat(uri): improve documentation and API

--- src/MFRC522v2-NDEF/uri.hpp
+++ src/MFRC522v2-NDEF/uri.hpp
@@ -4,28 +4,72 @@
 
 #define NDEF_URI_CODE_IDENTIFIER_COUNT 36
 
-struct NdefUriPrefix
-{
-    uint8_t code;
-    const char *prefix;
-};
-
+/**
+ * @brief NDEF URI payload builder
+ * @ref NFCForum-TS-RTD_URI_1.0.pdf
+ */
 class NdefUriPayload
 {
   public:
-    NdefUriPayload(const uint8_t *data, uint8_t length) : data(data), length(length){};
-    NdefUriPayload() : data(nullptr), length(0){};
-    NdefUriPayload(const NdefUriPayload &other) :
-        data(other.data), length(other.length){};
+    /**
+     * @brief Create a NDEF URI payload from a data array
+     * @param data The data array
+     * @param length The length of the data array
+     * @return A new NDEF URI payload object
+     * @note The data array is owned by the NDEF URI payload object
+     */
+    NdefUriPayload(const uint8_t *data, uint8_t length) :
+        _data(data), _length(length){};
+
+    // NdefUriPayload() : _data(nullptr), _length(0){};
+    // NdefUriPayload(const NdefUriPayload &other) :
+    //     _data(other._data), _length(other._length){};
+
+    /**
+     * @brief Create a NDEF URI payload from a URI string
+     * @param uri The URI string
+     * @return A new NDEF URI payload object
+     */
     NdefUriPayload(const char *uri);
-    ~NdefUriPayload() { delete[] data; };
+
+    /**
+     * @brief Destroy the NDEF URI payload object
+     */
+    ~NdefUriPayload() { delete[] _data; };
+
+    /**
+     * @brief Check if the NDEF URI payload is valid
+     * @return true if the NDEF URI payload is valid
+     */
     bool is_valid() const;
-    uint8_t get_code() const;
-    const uint8_t *get_data() const { return data; };
-    uint8_t get_length() const { return length; };
-    const char *get_uri() const;
+
+    /**
+     * @brief Get the URI identifier code
+     * @return The identifier code
+     */
+    uint8_t code() const;
+
+    /**
+     * @brief Get the URI data
+     * @return The URI data
+     */
+    const uint8_t *data() const { return _data; };
+
+    /**
+     * @brief Get the URI data length
+     * @return The URI data length
+     */
+    uint8_t length() const { return _length; };
+
+    /**
+     * @brief Get the URI string
+     * @return The URI string
+     * @note The URI string memory is automtically allocated but not owned by the NDEF
+     * URI payload object
+     */
+    const char *uri() const;
 
   private:
-    const uint8_t *data;
-    uint8_t length;
+    const uint8_t *_data;
+    uint8_t _length;
 };
--- tests/UriTest/UriTest.ino
+++ tests/UriTest/UriTest.ino
@@ -7,82 +7,79 @@
 test(build_ndef_uri_payload__simple)
 {
   NdefUriPayload payload("http://www.nfc.com");
-  assertEqual(payload.get_length(), (uint8_t)8);
-  const uint8_t *data = payload.get_data();
-  assertEqual(data[0], (uint8_t)0x01); // http://www.
-  assertEqual(data[1], (uint8_t)'n');
-  assertEqual(data[2], (uint8_t)'f');
-  assertEqual(data[3], (uint8_t)'c');
-  assertEqual(data[4], (uint8_t)'.');
-  assertEqual(data[5], (uint8_t)'c');
-  assertEqual(data[6], (uint8_t)'o');
-  assertEqual(data[7], (uint8_t)'m');
+  assertEqual(payload.length(), (uint8_t)8);
+  assertEqual(payload.data()[0], (uint8_t)0x01); // http://www.
+  assertEqual(payload.data()[1], (uint8_t)'n');
+  assertEqual(payload.data()[2], (uint8_t)'f');
+  assertEqual(payload.data()[3], (uint8_t)'c');
+  assertEqual(payload.data()[4], (uint8_t)'.');
+  assertEqual(payload.data()[5], (uint8_t)'c');
+  assertEqual(payload.data()[6], (uint8_t)'o');
+  assertEqual(payload.data()[7], (uint8_t)'m');
 }
 
 // NFCForum-TS-RTD_URI_1.0.pdf - A.2
 test(build_ndef_uri_payload__phone)
 {
   NdefUriPayload payload("tel:+35891234567");
-  assertEqual(payload.get_length(), (uint8_t)13);
-  const uint8_t *data = payload.get_data();
-  assertEqual(data[0], (uint8_t)0x05); // tel:
-  assertEqual(data[1], (uint8_t)'+');
-  assertEqual(data[2], (uint8_t)'3');
-  assertEqual(data[3], (uint8_t)'5');
-  assertEqual(data[4], (uint8_t)'8');
-  assertEqual(data[5], (uint8_t)'9');
-  assertEqual(data[6], (uint8_t)'1');
-  assertEqual(data[7], (uint8_t)'2');
-  assertEqual(data[8], (uint8_t)'3');
-  assertEqual(data[9], (uint8_t)'4');
-  assertEqual(data[10], (uint8_t)'5');
-  assertEqual(data[11], (uint8_t)'6');
-  assertEqual(data[12], (uint8_t)'7');
+  assertEqual(payload.length(), (uint8_t)13);
+  assertEqual(payload.data()[0], (uint8_t)0x05); // tel:
+  assertEqual(payload.data()[1], (uint8_t)'+');
+  assertEqual(payload.data()[2], (uint8_t)'3');
+  assertEqual(payload.data()[3], (uint8_t)'5');
+  assertEqual(payload.data()[4], (uint8_t)'8');
+  assertEqual(payload.data()[5], (uint8_t)'9');
+  assertEqual(payload.data()[6], (uint8_t)'1');
+  assertEqual(payload.data()[7], (uint8_t)'2');
+  assertEqual(payload.data()[8], (uint8_t)'3');
+  assertEqual(payload.data()[9], (uint8_t)'4');
+  assertEqual(payload.data()[10], (uint8_t)'5');
+  assertEqual(payload.data()[11], (uint8_t)'6');
+  assertEqual(payload.data()[12], (uint8_t)'7');
 }
 
 // NFCForum-TS-RTD_URI_1.0.pdf - A.3
 test(build_ndef_uri_payload__proprietary)
 {
   NdefUriPayload payload("mms://example.com/download.wmv");
-  assertEqual(payload.get_length(), (uint8_t)31);
-  const uint8_t *data = payload.get_data();
-  assertEqual(data[0], (uint8_t)0x00); // No abbreviation
-  assertEqual(data[1], (uint8_t)'m');
-  assertEqual(data[2], (uint8_t)'m');
-  assertEqual(data[3], (uint8_t)'s');
-  assertEqual(data[4], (uint8_t)':');
-  assertEqual(data[5], (uint8_t)'/');
-  assertEqual(data[6], (uint8_t)'/');
-  assertEqual(data[7], (uint8_t)'e');
-  assertEqual(data[8], (uint8_t)'x');
-  assertEqual(data[9], (uint8_t)'a');
-  assertEqual(data[10], (uint8_t)'m');
-  assertEqual(data[11], (uint8_t)'p');
-  assertEqual(data[12], (uint8_t)'l');
-  assertEqual(data[13], (uint8_t)'e');
-  assertEqual(data[14], (uint8_t)'.');
-  assertEqual(data[15], (uint8_t)'c');
-  assertEqual(data[16], (uint8_t)'o');
-  assertEqual(data[17], (uint8_t)'m');
-  assertEqual(data[18], (uint8_t)'/');
-  assertEqual(data[19], (uint8_t)'d');
-  assertEqual(data[20], (uint8_t)'o');
-  assertEqual(data[21], (uint8_t)'w');
-  assertEqual(data[22], (uint8_t)'n');
-  assertEqual(data[23], (uint8_t)'l');
-  assertEqual(data[24], (uint8_t)'o');
-  assertEqual(data[25], (uint8_t)'a');
-  assertEqual(data[26], (uint8_t)'d');
-  assertEqual(data[27], (uint8_t)'.');
-  assertEqual(data[28], (uint8_t)'w');
-  assertEqual(data[29], (uint8_t)'m');
-  assertEqual(data[30], (uint8_t)'v');
+  assertEqual(payload.length(), (uint8_t)31);
+  assertEqual(payload.data()[0], (uint8_t)0x00); // No abbreviation
+  assertEqual(payload.data()[1], (uint8_t)'m');
+  assertEqual(payload.data()[2], (uint8_t)'m');
+  assertEqual(payload.data()[3], (uint8_t)'s');
+  assertEqual(payload.data()[4], (uint8_t)':');
+  assertEqual(payload.data()[5], (uint8_t)'/');
+  assertEqual(payload.data()[6], (uint8_t)'/');
+  assertEqual(payload.data()[7], (uint8_t)'e');
+  assertEqual(payload.data()[8], (uint8_t)'x');
+  assertEqual(payload.data()[9], (uint8_t)'a');
+  assertEqual(payload.data()[10], (uint8_t)'m');
+  assertEqual(payload.data()[11], (uint8_t)'p');
+  assertEqual(payload.data()[12], (uint8_t)'l');
+  assertEqual(payload.data()[13], (uint8_t)'e');
+  assertEqual(payload.data()[14], (uint8_t)'.');
+  assertEqual(payload.data()[15], (uint8_t)'c');
+  assertEqual(payload.data()[16], (uint8_t)'o');
+  assertEqual(payload.data()[17], (uint8_t)'m');
+  assertEqual(payload.data()[18], (uint8_t)'/');
+  assertEqual(payload.data()[19], (uint8_t)'d');
+  assertEqual(payload.data()[20], (uint8_t)'o');
+  assertEqual(payload.data()[21], (uint8_t)'w');
+  assertEqual(payload.data()[22], (uint8_t)'n');
+  assertEqual(payload.data()[23], (uint8_t)'l');
+  assertEqual(payload.data()[24], (uint8_t)'o');
+  assertEqual(payload.data()[25], (uint8_t)'a');
+  assertEqual(payload.data()[26], (uint8_t)'d');
+  assertEqual(payload.data()[27], (uint8_t)'.');
+  assertEqual(payload.data()[28], (uint8_t)'w');
+  assertEqual(payload.data()[29], (uint8_t)'m');
+  assertEqual(payload.data()[30], (uint8_t)'v');
 }
 
 test(build_uri_from_ndef_uri_payload__simple)
 {
   const uint8_t *data = new uint8_t[8]{0x01, 'n', 'f', 'c', '.', 'c', 'o', 'm'};
-  const char *uri = NdefUriPayload(data, (uint8_t)8).get_uri();
+  const char *uri = NdefUriPayload(data, (uint8_t)8).uri();
   assertEqual(uri, "http://www.nfc.com");
   delete[] uri;
 }
@@ -91,7 +88,7 @@ test(build_uri_from_ndef_uri_payload__phone)
 {
   const uint8_t *data =
       new uint8_t[13]{0x05, '+', '3', '5', '8', '9', '1', '2', '3', '4', '5', '6', '7'};
-  const char *uri = NdefUriPayload(data, (uint8_t)13).get_uri();
+  const char *uri = NdefUriPayload(data, (uint8_t)13).uri();
   assertEqual(uri, "tel:+35891234567");
   delete[] uri;
 }
@@ -102,7 +99,7 @@ test(build_uri_from_ndef_uri_payload__proprietary)
       new uint8_t[31]{0x00, 'm', 'm', 's', ':', '/', '/', 'e', 'x', 'a', 'm',
                       'p',  'l', 'e', '.', 'c', 'o', 'm', '/', 'd', 'o', 'w',
                       'n',  'l', 'o', 'a', 'd', '.', 'w', 'm', 'v'};
-  const char *uri = NdefUriPayload(data, (uint8_t)31).get_uri();
+  const char *uri = NdefUriPayload(data, (uint8_t)31).uri();
   assertEqual(uri, "mms://example.com/download.wmv");
   delete[] uri;
 }
